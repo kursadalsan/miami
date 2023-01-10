66f7ea97cf1970f691105de87157f94f27da4b39
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2023-01-10 23:27:07 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ cd $1
 amass enum -passive -d $1 > 1.txt
 subfinder -d $1 > 2.txt
 cat *.txt > subs.txt
-httpx -l subs.txt -o http-subs.txt -t 150
+httpx-toolkit -l subs.txt -o http-subs.txt -t 150
 nuclei -rl 250 -bs 50 -s low -s medium -s high -s critical -l http-subs.txt
