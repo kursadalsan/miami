1960b55cc8e3620425ee370c0fe82c9963864007
kursadalsan <108494826+kursadalsan@users.noreply.github.com>
2023-01-10 23:03:55 +0300

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir $1 
+cd $1
+amass enum -passive -d $1 > 1.txt
+subfinder -d $1 > 2.txt
+cat *.txt > subs.txt
+httpx -l subs.txt -o http-subs.txt -t 150
+nuclei -rl 250 -bs 50 -s low -s medium -s high -s critical -l http-subs.txt
